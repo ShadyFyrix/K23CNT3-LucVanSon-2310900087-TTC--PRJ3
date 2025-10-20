8e4c0d75c41d6ebdd92d4297084691ba9e7c94dc
LucVanSon20 <shadyfyrix@gmail.com>
2025-10-20 20:19:07 +0700

    first look in day 1 with project manven

---
+++ demo.day01/demo.day01/src/main/java/com/shady/demo/day01/Application.java
@@ -0,0 +1,14 @@
+package com.shady.demo.day01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("LEN ĐÊ");
+	}
+
+}
